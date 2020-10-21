9fbde83e0bd6e1b6855fe6d12c260a139569b494
shahules786 <shahules786@gmail.com>
2020-10-21 17:12:33 +0530

    setup

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ import setuptools
 current_dir = os.path.dirname(os.path.abspath("__file__"))
 
 
-## requirements
-README = (current_dir / "README.md").read_text()
+# Get the long description from the README file
+with open(os.path.join(current_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 
 # What packages are required for this module to be executed?
@@ -22,7 +23,7 @@ setuptools.setup(
     author="Shahul ES",
     author_email="shahules786@gmail.com",
     description="A python package for sentiment analysis written using pytorch framework",
-    long_description_content_type=README,
+    long_description_content_type=long_description,
     url="https://github.com/shahules786/Twitter-Sentiment",
     packages=setuptools.find_packages(),
     classifiers=[
